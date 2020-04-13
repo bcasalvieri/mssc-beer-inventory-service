b48915d0de14e95ad6cfd4e0ca725c8063431c4f
bcasalvieri <bmcasalvieri@gmail.com>
2020-04-13 12:23:11 -0400

    finishing challenge for setting up new inventory listener

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-13T11:30:32-0400",
+    date = "2020-04-13T12:10:37-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 @Component
